c1fdc5cf5f343712909ee1fddb5cda9161770f7c
JrJoseLs <junior_er@hotmail.es>
2023-08-21 19:25:44 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './ui/nav-bar/nav-bar.component';
+import { BannerComponent } from './ui/banner/banner.component';
+import { HeaderComponent } from './ui/header/header.component';
+import { AboutComponent } from './ui/about/about.component';
+import { SkillsComponent } from './ui/skills/skills.component';
+import { ProjectsComponent } from './ui/projects/projects.component';
+import { ContactComponent } from './ui/contact/contact.component';
+import { FooterComponent } from './ui/footer/footer.component';
+import { CurriculumComponent } from './ui/curriculum/curriculum.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    BannerComponent,
+    HeaderComponent,
+    AboutComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    ContactComponent,
+    FooterComponent,
+    CurriculumComponent
   ],
   imports: [
     BrowserModule,
